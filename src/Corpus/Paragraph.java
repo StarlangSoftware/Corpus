019ebf107c0a89844c61817f42a099cb2920caf9
Selen <parlarselen@gmail.com>
2018-06-19 11:30:34 +0300

    added comments and examples for documentation

--- src/Corpus/Paragraph.java
+++ src/Corpus/Paragraph.java
@@ -6,19 +6,38 @@ public class Paragraph {
 
     private ArrayList<Sentence> sentences;
 
-    public Paragraph(){
+    /**
+     * A constructor of {@link Paragraph} class which creates an {@link ArrayList} sentences.
+     */
+    public Paragraph() {
         sentences = new ArrayList<Sentence>();
     }
 
-    public void addSentence(Sentence s){
+    /**
+     * The addSentence method adds given sentence to sentences {@link ArrayList}.
+     *
+     * @param s Sentence type input to add sentences.
+     */
+    public void addSentence(Sentence s) {
         sentences.add(s);
     }
 
-    public int sentenceCount(){
+    /**
+     * The sentenceCount method finds the size of the {@link ArrayList} sentences.
+     *
+     * @return the size of the {@link ArrayList} sentences.
+     */
+    public int sentenceCount() {
         return sentences.size();
     }
 
-    public Sentence getSentence(int index){
+    /**
+     * The getSentence method finds the sentence from sentences {@link ArrayList} at given index.
+     *
+     * @param index used to get a sentence.
+     * @return sentence at given index.
+     */
+    public Sentence getSentence(int index) {
         return sentences.get(index);
     }
 
