d026c750dbc2167056088401f7a58e462842b965
Selen <parlarselen@gmail.com>
2018-06-19 11:34:56 +0300

    added comments and examples for documentation

--- src/Corpus/WordFormat.java
+++ src/Corpus/WordFormat.java
@@ -1,5 +1,20 @@
 package Corpus;
 
 public enum WordFormat {
-    SURFACE, LETTER_2, LETTER_3, LETTER_4
+    /**
+     * Surface/Original form
+     */
+    SURFACE,
+    /**
+     * Create 2-Gram words as output.
+     */
+    LETTER_2,
+    /**
+     * Create 3-Gram words as output.
+     */
+    LETTER_3,
+    /**
+     * Create 4-Gram words as output.
+     */
+    LETTER_4
 }
